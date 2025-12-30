7bef4437eac1c1678b197a7cf36362276f6dda6c
Subrath Sapkota <subrathsapkota119@gmail.com>
2025-12-30 10:41:40 +0545

    db config and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import path from 'path';
-import { ENV } from './config/env.js'
+import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
+import { clerkMiddleware } from '@clerk/express'
+
 
 const app = express();
 
 const __dirname = path.resolve();
+
+app.use(clerkMiddleware())
+
 
 app.get('/app/health', (req, res) => {
   res.status(200).json({ message: 'Success' });
@@ -18,4 +24,8 @@ if (ENV.NODE_ENV === 'production') {
   });
 }
 
-app.listen(ENV.PORT, () => console.log('Server is up in prot 3000'));
+app.listen(ENV.PORT, () => {
+  console.log('Server is up and running');
+  connectDB();
+  
+});
